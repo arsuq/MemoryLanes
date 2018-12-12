12cc2c072101f495880a2ce4ebfb776ccd8f7240
arsuq <arsuq@ryzen.home>
2018-12-12 16:32:13 +0200

    ..

---
+++ Tests/Surface/ClosingLane.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TestRunner;
+
+namespace Tests.Surface
+{
+	public class ClosingLane : ITestSurface
+	{
+		public string Info => "Tests lane skipping.";
+
+		public string FailureMessage { get; private set; }
+		public bool? Passed { get; private set; }
+		public bool RequireArgs => false;
+		public bool IsComplete { get; private set; }
+
+		public async Task Run(IDictionary<string, List<string>> args)
+		{
+			if (args.ContainsKey("-all"))
+				args.Add("-store", new List<string>() { "mh", "mmf", "nh" });
+
+			args.AssertAll("-store");
+			var opt = args["-store"];
+			opt.AssertNothingButTheseArgs("mh", "mmf", "nh");
+
+			var stg = new MemoryLaneSettings(2000, 8, 10000);
+			var iH = new Dictionary<string, IHighway>();
+
+			iH.Add("mh", new HeapHighway(stg, 2000, 2000, 2000));
+			iH.Add("nh", new MarshalHighway(stg, 2000, 2000, 2000));
+			iH.Add("mmf", new MappedHighway(stg, 2000, 2000, 2000));
+
+			foreach (var kp in iH)
+			{
+				var hwName = kp.Value.GetType().Name;
+				var F = new List<MemoryFragment>();
+
+				if (opt.Contains(kp.Key))
+				{
+					var hw = kp.Value;
+					using (hw)
+					{
+						var lane1 = hw.GetLanes()[1];
+						var lane2 = hw.GetLanes()[2];
+						lane1.Force(true);
+
+						F.Add(hw.AllocFragment(1500));
+						F.Add(hw.AllocFragment(1500));
+
+						try
+						{
+							var af = hw.GetTotalActiveFragments();
+							if (af != 2)
+							{
+								Passed = false;
+								FailureMessage = string.Format("{0}: expected 2 active fragments, got {1}", hwName, af);
+								return;
+							}
+							if (lane1.Allocations > 0)
+							{
+								Passed = false;
+								FailureMessage = string.Format("{0}: the lane #1 was force closed, it should have 0 fragments, got {1}", hwName, lane1.Allocations);
+								return;
+							}
+							if (lane2.Allocations < 1)
+							{
+								Passed = false;
+								FailureMessage = string.Format("{0}: the lane #2 should have at least one allocation, found {1}", hwName, lane2.Allocations);
+								return;
+							}
+
+							Print.Trace("{0}: closing a lane works as expected", ConsoleColor.Green, null, hwName);
+
+							lane1.Force(false);
+							// should go into lane1 because lane0 has 1500/2000 
+							F.Add(hw.AllocFragment(1500));
+
+							if (lane1.Allocations != 1)
+							{
+								Passed = false;
+								FailureMessage = string.Format("{0}: the lane #1 was force opened, it should have 1 fragments, got {1}", hwName, lane1.Allocations);
+								return;
+							}
+						}
+						finally
+						{
+							foreach (var f in F) f.Dispose();
+						}
+					}
+				}
+			}
+
+			if (!Passed.HasValue) Passed = true;
+			IsComplete = true;
+		}
+	}
+}
