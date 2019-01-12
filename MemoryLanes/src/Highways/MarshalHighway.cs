aef325af7282fd873ee188211bff7f0dff2dd199
arsuq <arsuq@ryzen.home>
2019-01-12 11:10:25 +0200

    MPL2 License

--- MemoryLanes/src/Highways/MarshalHighway.cs
+++ MemoryLanes/src/Highways/MarshalHighway.cs
@@ -1,4 +1,8 @@
-﻿namespace System
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+namespace System
 {
 	/// <summary>
 	/// Allocates memory lanes via Marshal.AllocHGlobal
